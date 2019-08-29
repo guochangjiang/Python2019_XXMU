2fca24b5d8f8010541e9a116e2b6d280cf4d4fe3
Guo Changjiang <guochangjiang@users.noreply.github.com>
2019-08-29 10:46:23 +0800

    Add files via upload
    第01章课程源代码

---
+++ 实例源代码/第01章/1.1CircleArea.py
@@ -0,0 +1,6 @@
+radius = 25                     # 圆的半径是25
+pi = 3.1415                     # π值
+area = pi * radius * radius     # 圆的面积公式
+print(area)
+print("%.2f" % area)            # 只输出两位小数
+print("{:.2f}".format(area))
---
+++ 实例源代码/第01章/1.4CircleDraw.py
@@ -0,0 +1,6 @@
+import turtle       # 引用turtle库
+turtle.pensize(2)   # 设置画笔宽度为2像素
+turtle.circle(10)   # 绘制半径为10像素的圆
+turtle.circle(40)   # 绘制半径为40像素的圆
+turtle.circle(80)   # 绘制半径为80像素的圆
+turtle.circle(160)  # 绘制半径为160像素的圆
