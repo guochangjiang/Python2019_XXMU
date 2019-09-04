74869666219f2859e4380205b901dfdcf5eadb5c
Guo Changjiang <guochangjiang@users.noreply.github.com>
2019-09-04 11:27:19 +0800

    Update 1.5RedStar.py

--- 作业/第01章/1.5RedStar.py
+++ 作业/第01章/1.5RedStar.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# 1.5RedStar.py
 
 """
 1.5 五角星的绘制
@@ -7,17 +8,14 @@
 
 import turtle
 
-t=turtle.Pen()  #获得画笔
-t.hideturtle()  #隐藏海龟
-t.pencolor("red")
-t.fillcolor("red")
-t.begin_fill()
+turtle.pencolor("red")
+turtle.begin_fill()
 count = 1
 while count <= 5:
-    t.forward(200)  # 向前走200
-    t.right(144)    # 向右转144度
+    turtle.forward(200)  # 向前走200
+    turtle.right(144)    # 向右转144度
     count += 1
-t.end_fill()
-ts = turtle.getscreen()
-ts.getcanvas().postscript(file="RedStar.eps")
+turtle.fillcolor("red")
+turtle.end_fill()
+turtle.hideturtle()     # 隐藏海龟
 turtle.done()
