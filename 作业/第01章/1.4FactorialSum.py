ccf500dd95dacda79eaf9fa51fead82d84a25232
Guo Changjiang <guochangjiang@users.noreply.github.com>
2019-09-09 09:26:05 +0800

    Update 1.4FactorialSum.py

--- 作业/第01章/1.4FactorialSum.py
+++ 作业/第01章/1.4FactorialSum.py
@@ -3,3 +3,13 @@
 """
 1.4 计算1!+2!+3!+…+10!
 """
+
+
+sum = 0
+for i in range(1, 11):
+    factorial = 1
+    for j in range(1, i+1):
+        factorial *= j
+    sum += factorial
+
+print("1!+2!+3!+…+10!=", sum)
